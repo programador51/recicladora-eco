6e6ab0cee262b98f50321f953b6c585bab835da0
programador51 <gskitcat@gmail.com>
2025-10-19 16:39:17 -0600

    portada

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,19 +2,19 @@ import Layout from './structure/Layout'
 import { Typography, Stack } from '@mui/material'
 import fime from '../src/fime.jpg'
 import uanl from '../src/uanl.png'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
 
 const rows = [
   {
-    matricula:'1731168',
-    nombre:'José Luis Pérez Olguín'
+    matricula: '1731168',
+    nombre: 'José Luis Pérez Olguín'
   }
-];
+]
 
 function App(): React.JSX.Element {
   // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
@@ -24,47 +24,59 @@ function App(): React.JSX.Element {
   return (
     <Layout>
       <Stack alignItems={'center'}>
-        <Stack flexDirection={'row'} alignItems={'center'} gap={4} style={{
-          marginBottom:"70px"
-        }}>
+        <Stack
+          flexDirection={'row'}
+          alignItems={'center'}
+          gap={4}
+          style={{
+            marginBottom: '70px'
+          }}
+        >
           <img height={'70px'} src={fime} alt="FIME" />
 
-          <div style={{margin:0}}>
-            <Typography textAlign={'center'} margin={0}>Universidad Autónoma de Nuevo León</Typography>
-            <Typography textAlign={'center'} margin={0}>Facultad de ingeniería Mecánica y Eléctrica</Typography>
+          <div style={{ margin: 0 }}>
+            <Typography textAlign={'center'} margin={0}>
+              Universidad Autónoma de Nuevo León
+            </Typography>
+            <Typography textAlign={'center'} margin={0}>
+              Facultad de ingeniería Mecánica y Eléctrica
+            </Typography>
           </div>
           <img height={'70px'} src={uanl} alt="UANL" />
         </Stack>
         <Typography textAlign={'center'}>Diseño y especificación de software</Typography>
-        <Typography variant='body2' fontStyle={'italic'} color='textSecondary' textAlign={'center'}>Agosto - Diciembre 2025</Typography>
+        <Typography variant="body2" fontStyle={'italic'} color="textSecondary" textAlign={'center'}>
+          Agosto - Diciembre 2025
+        </Typography>
 
-         <TableContainer>
-      <Table sx={{ width: "50%", justifyContent:'center',margin:'40px 25%' }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align='center'>Nombre</TableCell>
-            <TableCell align="center">Matricula</TableCell>
-            <TableCell align="center">Carrera</TableCell>
-            
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.matricula}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.nombre}
-              </TableCell>
-              <TableCell align="center">{row.matricula}</TableCell>
-              <TableCell align="center">IAS</TableCell>
-              
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        <TableContainer>
+          <Table
+            sx={{ width: '50%', justifyContent: 'center', margin: '40px 25%' }}
+            aria-label="simple table"
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">Nombre</TableCell>
+                <TableCell align="center">Matricula</TableCell>
+                <TableCell align="center">Carrera</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.matricula}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.nombre}
+                  </TableCell>
+                  <TableCell align="center">{row.matricula}</TableCell>
+                  <TableCell align="center">IAS</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
 
         <Stack flexDirection={'row'} gap={1} alignItems={'center'}>
           <Typography fontWeight={'bold'}>Grupo:</Typography>
@@ -90,7 +102,9 @@ function App(): React.JSX.Element {
           <Typography>#1</Typography>
         </Stack>
 
-        <Typography style={{margin:"150px 0 0 0"}}>Ciudad Universitaria, San Nicolás de los Garza, Nuevo León</Typography>
+        <Typography style={{ margin: '150px 0 0 0' }}>
+          Ciudad Universitaria, San Nicolás de los Garza, Nuevo León
+        </Typography>
       </Stack>
     </Layout>
   )
--- src/renderer/src/structure/Nav/index.tsx
+++ src/renderer/src/structure/Nav/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router'
-import { Button, Typography } from '@mui/material'
+import { Button, Typography, Stack } from '@mui/material'
+import HomeIcon from '@mui/icons-material/Home'
+import IconButton from '@mui/material/IconButton'
 import { cyan } from '@mui/material/colors'
-import {
-  
-  ganancias,
-  gestion,
-  impacto,
-  logistica,
-  reportes,
-  ventas
-} from '@renderer/routes'
+import { ganancias, gestion, impacto, logistica, reportes, ventas } from '@renderer/routes'
 
 export default function Nav(): React.JSX.Element {
   const links = [
@@ -25,15 +19,28 @@ export default function Nav(): React.JSX.Element {
 
   return (
     <>
-      <Typography
-        margin={'0 0 20px 0'}
-        variant="h6"
-        sx={{ color: cyan[50] }}
-        textAlign={'center'}
-        fontWeight={'bold'}
+      <Stack
+        flexDirection={'row'}
+        gap={1}
+        alignItems={'center'}
+        marginBottom={3}
+        justifyContent={'center'}
       >
-        Dashboard
-      </Typography>
+        <NavLink to={'/'}>
+        <IconButton>
+          <HomeIcon color="primary" />
+        </IconButton>
+        </NavLink>
+        <Typography
+          // margin={'0 0 20px 0'}
+          variant="h6"
+          sx={{ color: cyan[50] }}
+          textAlign={'center'}
+          fontWeight={'bold'}
+        >
+          Dashboard
+        </Typography>
+      </Stack>
 
       {links.map((link) => (
         <NavLink key={link.path} to={`/${link.path}`} style={{ textDecoration: 'none' }}>
